41077fceb2016d58bded0229c56ac4f7bdf5cb21
Liam Coleman <19TC.LiamColeman@MSLetb.ie>
2021-05-04 16:26:30 +0100

    pattern 1 midway

--- WinRetail_Auto_Task_Utility/PasteBox.cs
+++ WinRetail_Auto_Task_Utility/PasteBox.cs
@@ -52,6 +52,51 @@ namespace WinRetail_Auto_Task_Utility
             File.WriteAllLines(pattern_1_output, omit_line);
 
 
+            //pull out pattern 1
+            List<string> Lines_from_what_user_pasted_in = new List<string>(File.ReadAllLines(pattern_1_output));
+            List<string> Pattern_1_List = new List<string>();
+            for (int i = 0; i < Lines_from_what_user_pasted_in.Count; i++)
+            {
+                if (Lines_from_what_user_pasted_in[i].ToString().Contains("Serial No:"))
+                {
+                    Pattern_1_List.Add(Lines_from_what_user_pasted_in[i - 2]
+                        +","
+                        + (Lines_from_what_user_pasted_in[i - 1])+
+                        ","
+                        + Lines_from_what_user_pasted_in[i]);
+                    
+                    
+                    
+                }
+                
+            }
+            using (StreamWriter sr = new StreamWriter(pattern_1_output))
+            {
+
+                Pattern_1_List.ForEach(i => sr.Write("{0}\n", i));
+
+            }
+
+            // output looks like this.....
+            //WIN10 LICENCE                      75.00,  0000000000784,Serial No:       04248000841821
+            //WIN10 LICENCE                      75.00,  0000000000784,Serial No:       04248000841830
+            //WIN10 LICENCE                      75.00,  0000000000784,Serial No:       04248000841831
+            //WIN10 LICENCE                      75.00,  0000000000784,Serial No:       04248000841832
+
+            ///parse pattern 1 and add it datamodel
+            ///
+
+            string line_from_pattern_1_output;
+            
+            using (StreamReader sr = new StreamReader(pattern_1_output))
+            {
+                while ((line_from_pattern_1_output = sr.ReadLine())! = null)
+                {
+                    string[] fields = line_from_pattern_1_output.Split(','); 
+
+                }
+            }
+
 
 
 
