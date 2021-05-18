4e6477ab3b72c4d138fd18c6d23220f952d96776
Liam Coleman <19TC.LiamColeman@MSLetb.ie>
2021-05-18 12:33:32 +0100

    Pastebox GUI remider to get barcodes from winretail.

--- WinRetail_Auto_Task_Utility/PasteBox.Designer.cs
+++ WinRetail_Auto_Task_Utility/PasteBox.Designer.cs
@@ -28,17 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PasteBox));
             this.label1 = new System.Windows.Forms.Label();
             this.textBox_pasted_items = new System.Windows.Forms.TextBox();
             this.button_ok = new System.Windows.Forms.Button();
             this.button_exit = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(71, 50);
+            this.label1.Location = new System.Drawing.Point(33, 313);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(412, 24);
@@ -47,7 +52,7 @@
             // 
             // textBox_pasted_items
             // 
-            this.textBox_pasted_items.Location = new System.Drawing.Point(75, 81);
+            this.textBox_pasted_items.Location = new System.Drawing.Point(37, 339);
             this.textBox_pasted_items.Margin = new System.Windows.Forms.Padding(2);
             this.textBox_pasted_items.Multiline = true;
             this.textBox_pasted_items.Name = "textBox_pasted_items";
@@ -60,7 +65,7 @@
             // 
             this.button_ok.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
             this.button_ok.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button_ok.Location = new System.Drawing.Point(505, 198);
+            this.button_ok.Location = new System.Drawing.Point(496, 339);
             this.button_ok.Margin = new System.Windows.Forms.Padding(2);
             this.button_ok.Name = "button_ok";
             this.button_ok.Size = new System.Drawing.Size(67, 110);
@@ -73,7 +78,7 @@
             // 
             this.button_exit.BackColor = System.Drawing.Color.Red;
             this.button_exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button_exit.Location = new System.Drawing.Point(505, 454);
+            this.button_exit.Location = new System.Drawing.Point(496, 712);
             this.button_exit.Margin = new System.Windows.Forms.Padding(2);
             this.button_exit.Name = "button_exit";
             this.button_exit.Size = new System.Drawing.Size(67, 110);
@@ -82,12 +87,46 @@
             this.button_exit.UseVisualStyleBackColor = false;
             this.button_exit.Click += new System.EventHandler(this.button_exit_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 24);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(538, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Did You Check You Have the BarCodes From WinRetail?";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(549, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(218, 261);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(57, 288);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(679, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "=================================================================================" +
+    "===============================";
+            // 
             // PasteBox
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.AppWorkspace;
             this.ClientSize = new System.Drawing.Size(810, 862);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.button_exit);
             this.Controls.Add(this.button_ok);
             this.Controls.Add(this.textBox_pasted_items);
@@ -96,6 +135,7 @@
             this.Name = "PasteBox";
             this.Text = "PasteBox";
             this.Load += new System.EventHandler(this.PasteBox_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -107,5 +147,8 @@
         private System.Windows.Forms.TextBox textBox_pasted_items;
         private System.Windows.Forms.Button button_ok;
         private System.Windows.Forms.Button button_exit;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
     }
 }
