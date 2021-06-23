6eecf03e7a131d4313dddbf724b1f87fe55a9f63
Liam Coleman <19TC.LiamColeman@MSLetb.ie>
2021-06-23 11:28:53 +0100

    refund continued

--- WinRetail_Auto_Task_Utility/PasteBox.cs
+++ WinRetail_Auto_Task_Utility/PasteBox.cs
@@ -21,6 +21,7 @@ namespace WinRetail_Auto_Task_Utility
         string Company_name;
 
        public  List<Items_From_Receipt> Item_list_123 = new List<Items_From_Receipt>();
+        public List<refund_List> list_of_Refunds = new List<refund_List>();
 
         public List<Items_From_Receipt> get_list ()
         {
@@ -87,196 +88,238 @@ namespace WinRetail_Auto_Task_Utility
         {
             Lines = removeblanks(Lines);
             for (int i = 0; i < Lines.Count; i++)
+                if (Lines[i].Contains("..............REFUND ITEM...............") && Lines[i + 3].Contains("Serial No:"))
+                {
 
-            {
-                ///handle void/////////
-                if(Lines[i].ToString().Contains("VOID ITEM "))
-                {
-                    i++;
-                    Item_list_123[Item_list_123.Count - 1].status = "VOIDED";
-                    
-                   
-                    //new_list.Remove(Lines[i]);
-                }
-                if (Lines[i].ToString().Contains("Serial No:"))
-                {
-                    new_list.Add(Lines[i - 2] + "," + (Lines[i - 1] + "," + (Lines[i])));
+
                     Item_list_123.Add(new Items_From_Receipt
                     {
                         Config_item_ID = "",
-                        Product_Name = Lines[i - 2].Substring(0, 30),
+                        Product_Name = Lines[i + 1].Substring(0, 30),
                         Company_Name = Company_name,
                         Install_Date = "",
-                        Serial_Number = Lines[i].Substring(11).Trim(),
+                        Serial_Number = Lines[i + 3].Substring(11).Trim(),
                         Reference_Name = "",
-                        status=""
+                        status = "REFUNDED ITEM"
+
 
                     });
                 }
 
-                if (Lines[i].ToString().Contains("@"))
+                else if (Lines[i].Contains("..............REFUND ITEM...............") && !Lines[i + 3].Contains("Serial No:"))
                 {
-
-                    new_list.Add(Lines[i - 1] + "," + (Lines[i]) + "," + (Lines[i + 1]));
-
-                    string line = Lines[i].ToString();//2  @    195.00      390.00
-                    string[] aprts = line.Split('@');//2  @ 
-                    var range = aprts[0];//2
-                    int Frange = Convert.ToInt32(range);
-                    for (int qty = 1; qty <= Frange; qty++)///increment by frange
-                    {
-                        Item_list_123.Add(new Items_From_Receipt
-                        {
-
-                            Config_item_ID = "",
-                            Product_Name = Lines[i - 1],
-                            Company_Name = Company_name,
-                            Install_Date = "",
-                            Serial_Number = "",
-                            Reference_Name = "",
-                            status =""
-
-                        });
-                    }
-                }
-                if(Lines[i].Contains("--------------"))
-                {
-                    break;
-                }
-
-                if (Lines[i].Trim().Split(' ').Length==1  &&
-                    Lines[i].Trim().Split(' ')[0].Length>0 &&
-                    !Lines[i-1].Contains("@") && 
-                    !Lines[i+1].Contains("Serial No"))
-                {
-                    //USB KEYBOARDS                       1.00
-                    //  0000000000256
-                    //USB MOUSE                           1.50
-                    //  0000000000257////
-                    //24" SCREEN                          3.00
-                    //  0000000000300
-                    //Serial No:       ZZQ1H4LNC02097
-                    //WIN 10 64 - BIT                       6.00
-                    //  0000000000594
-                    //Serial No:          QTC20201246
-                    //2 KVA UPS                           8.00
-                    //  0000000000187
-                    //Serial No:             20040004
-                    //1MTR PATCH
-                    //              6  @      1.00        6.00
-                    //  0000000000334
-                    // most likely a PLU Line
-                    // check if line above has an @ sign or
-                    // if the line below has Serial No:
-                    // if it doesn't have one of these then it is a
-                    // single item to be added
-                    // taking the description from the line above
-                    // see USB KEYBOARDS / USB MOUSE sample above
                     Item_list_123.Add(new Items_From_Receipt
                     {
-
                         Config_item_ID = "",
-                        Product_Name = Lines[i - 1].Substring(0,20).Trim(),
+                        Product_Name = Lines[i + 1].Substring(0, 30),
                         Company_Name = Company_name,
                         Install_Date = "",
                         Serial_Number = "",
                         Reference_Name = "",
-                        status=""
+                        status = "REFUNDED ITEM"
+
 
                     });
-
                 }
 
-                //if (Lines[i].ToString().Contains("  0"))
-                //{
-                //    new_list.Add(Lines[i - 1] + "," + (Lines[i]));
 
-                //    Item_list_123.Add(new Items_From_Receipt
-                //    {
-                //        Config_item_ID = "",
-                //        Product_Name = Lines[i - 1].Substring(0, 20),
-                //        Company_Name = Company_name,
-                //        Install_Date = "",
-                //        Serial_Number = "",
-                //        Reference_Name = ""
-
-                //    });
-
-                //}
-                
-
-
-            }
-        }
-
-        private List<string> removeblanks(List<string> lines)
-        {
-            List<string> ret_list = new List<string>();
-
-            foreach(string line in lines)
-            {
-                if (line.Trim().Length>0)
+                ///handle void/////////
+                else if (Lines[i].ToString().Contains("VOID ITEM "))
                 {
-                    if(line.Contains("   --------------"))
-                    {
-                        ret_list.Add("");
-                    }
-                    ret_list.Add(line);
+
+                    i++;
+                    Item_list_123[Item_list_123.Count - 1].status = "VOIDED";
+
+
+                    //new_list.Remove(Lines[i]);
                 }
-            }
-            ret_list.Add("");
-            return ret_list;
-        }
-
-        private void handle_pastbox_contents_skip_unwanted_text()
-        {
-            using (StreamReader SR = new StreamReader(contents_of_textbox))
-            {
-                string[] lines = File.ReadAllLines(contents_of_textbox);
-                {
-                    var target_text = lines.SkipWhile(x => x != "========================================")// skips everything before 
-                        .Skip(1)//and the ====== itself
-                                //.TakeWhile(x => x != "========================================")//up to =======
-                        .TakeWhile(x => x != "                          --------------")
-                        .ToList();
-
-
-                    using (StreamWriter SW = new StreamWriter(pattern_output))
+            else if (Lines[i].ToString().Contains("Serial No:"))
                     {
-
+                        new_list.Add(Lines[i - 2] + "," + (Lines[i - 1] + "," + (Lines[i])));
+                        Item_list_123.Add(new Items_From_Receipt
                         {
-                            target_text.ForEach(i => SW.Write("{0}\r", i));
+                            Config_item_ID = "",
+                            Product_Name = Lines[i - 2].Substring(0, 30),
+                            Company_Name = Company_name,
+                            Install_Date = "",
+                            Serial_Number = Lines[i].Substring(11).Trim(),
+                            Reference_Name = "",
+                            status = ""
+
+                        });
+                    }
+
+                    else if (Lines[i].ToString().Contains("@"))
+                    {
+
+                        new_list.Add(Lines[i - 1] + "," + (Lines[i]) + "," + (Lines[i + 1]));
+
+                        string line = Lines[i].ToString();//2  @    195.00      390.00
+                        string[] aprts = line.Split('@');//2  @ 
+                        var range = aprts[0];//2
+                        int Frange = Convert.ToInt32(range);
+                        for (int qty = 1; qty <= Frange; qty++)///increment by frange
+                        {
+                            Item_list_123.Add(new Items_From_Receipt
+                            {
+
+                                Config_item_ID = "",
+                                Product_Name = Lines[i - 1],
+                                Company_Name = Company_name,
+                                Install_Date = "",
+                                Serial_Number = "",
+                                Reference_Name = "",
+                                status = ""
+
+                            });
                         }
                     }
+                    else if (Lines[i].Contains("--------------"))
+                    {
+                        break;
+                    }
+
+                    else if (Lines[i].Trim().Split(' ').Length == 1 &&
+                        Lines[i].Trim().Split(' ')[0].Length > 0 &&
+                        !Lines[i - 1].Contains("@") &&
+                        !Lines[i + 1].Contains("Serial No"))
+                    {
+                        //USB KEYBOARDS                       1.00
+                        //  0000000000256
+                        //USB MOUSE                           1.50
+                        //  0000000000257////
+                        //24" SCREEN                          3.00
+                        //  0000000000300
+                        //Serial No:       ZZQ1H4LNC02097
+                        //WIN 10 64 - BIT                       6.00
+                        //  0000000000594
+                        //Serial No:          QTC20201246
+                        //2 KVA UPS                           8.00
+                        //  0000000000187
+                        //Serial No:             20040004
+                        //1MTR PATCH
+                        //              6  @      1.00        6.00
+                        //  0000000000334
+                        // most likely a PLU Line
+                        // check if line above has an @ sign or
+                        // if the line below has Serial No:
+                        // if it doesn't have one of these then it is a
+                        // single item to be added
+                        // taking the description from the line above
+                        // see USB KEYBOARDS / USB MOUSE sample above
+                        Item_list_123.Add(new Items_From_Receipt
+                        {
+
+                            Config_item_ID = "",
+                            Product_Name = Lines[i - 1].Substring(0, 20).Trim(),
+                            Company_Name = Company_name,
+                            Install_Date = "",
+                            Serial_Number = "",
+                            Reference_Name = "",
+                            status = ""
+
+                        });
+
+                    }
+
+
+
+                    //if (Lines[i].ToString().Contains("  0"))
+                    //{
+                    //    new_list.Add(Lines[i - 1] + "," + (Lines[i]));
+
+                    //    Item_list_123.Add(new Items_From_Receipt
+                    //    {
+                    //        Config_item_ID = "",
+                    //        Product_Name = Lines[i - 1].Substring(0, 20),
+                    //        Company_Name = Company_name,
+                    //        Install_Date = "",
+                    //        Serial_Number = "",
+                    //        Reference_Name = ""
+
+                    //    });
+
+                    //}
+
+
+
+
+
+                
+
+                
+        }
+
+            private List<string> removeblanks(List<string> lines)
+            {
+                List<string> ret_list = new List<string>();
+
+                foreach (string line in lines)
+                {
+                    if (line.Trim().Length > 0)
+                    {
+                        if (line.Contains("   --------------"))
+                        {
+                            ret_list.Add("");
+                        }
+                        ret_list.Add(line);
+                    }
                 }
+                ret_list.Add("");
+                return ret_list;
+            }
+
+            private void handle_pastbox_contents_skip_unwanted_text()
+            {
+                using (StreamReader SR = new StreamReader(contents_of_textbox))
+                {
+                    string[] lines = File.ReadAllLines(contents_of_textbox);
+                    {
+                        var target_text = lines.SkipWhile(x => x != "========================================")// skips everything before 
+                            .Skip(1)//and the ====== itself
+                                    //.TakeWhile(x => x != "========================================")//up to =======
+                            .TakeWhile(x => x != "                          --------------")
+                            .ToList();
+
+
+                        using (StreamWriter SW = new StreamWriter(pattern_output))
+                        {
+
+                            {
+                                target_text.ForEach(i => SW.Write("{0}\r", i));
+                            }
+                        }
+                    }
+
+                }
+            }
+
+
+
+
+
+
+            private void button_exit_Click(object sender, EventArgs e)
+            {
+                MessageBox.Show("Have You Finished Your Work?\nYou Sure you Want to Exit?", "Warning",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Close();
 
             }
-        }
+
+            private void PasteBox_Load(object sender, EventArgs e)
+            {
+
+            }
+
+            private void textBox_pasted_items_TextChanged(object sender, EventArgs e)
+            {
 
 
 
 
-
-
-        private void button_exit_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("Have You Finished Your Work?\nYou Sure you Want to Exit?", "Warning",
-            MessageBoxButtons.OK, MessageBoxIcon.Error);
-            this.Close();
-           
-        }
-
-        private void PasteBox_Load(object sender, EventArgs e)
-        {
-            
-        }
-
-        private void textBox_pasted_items_TextChanged(object sender, EventArgs e)
-        {
-            
-            
-
-
-        }
+            }
+        
+        
     }
 }
---
+++ WinRetail_Auto_Task_Utility/refund_List.cs
@@ -0,0 +1,15 @@
+﻿namespace WinRetail_Auto_Task_Utility
+{
+    public class refund_List
+    {
+        public int rowIndex { get; set; }
+        public string Config_item_ID { get; set; }
+        public string Product_Name { get; set; }
+        public string Company_Name { get; set; }
+        public string Install_Date { get; set; }
+        public string Warranty_Expiration { get; set; }
+        public string Serial_Number { get; set; }
+        public string Reference_Name { get; set; }
+        public string status { get; set; }
+    }
+}
